5d21cc36852bb50ef97d0e9bec64ff8dc117923e
MasakiFukunishi <fmsknn@gmail.com>
2021-04-11 17:51:19 +0900

    next updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,36 +21,39 @@ class MyDocument extends Document {
           {/* windows */}
           <meta
             name="msapplication-square70x70logo"
-            content="/site-tile-70x70.png"
+            content="/favicons/site-tile-70x70.png"
           />
           <meta
             name="msapplication-square150x150logo"
-            content="/site-tile-150x150.png"
+            content="/favicons/site-tile-150x150.png"
           />
           <meta
             name="msapplication-wide310x150logo"
-            content="/site-tile-310x150.png"
+            content="/favicons/site-tile-310x150.png"
           />
           <meta
             name="msapplication-square310x310logo"
-            content="/site-tile-310x310.png"
+            content="/favicons/site-tile-310x310.png"
           />
           <meta name="msapplication-TileColor" content="#000" />
           {/* safari */}
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="#000" />
-          <meta name="apple-mobile-web-app-title" content="myapp" />
+          <meta name="/faviconsapple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="/faviconsapple-mobile-web-app-status-bar-style"
+            content="#000"
+          />
+          <meta name="/faviconsapple-mobile-web-app-title" content="PWA LIST" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon-180x180.png"
+            href="/favicons/apple-touch-icon-180x180.png"
           />
           {/* common */}
-          <meta name="application-name" content="myapp" />
+          <meta name="application-name" content="PWA LIST" />
           <meta name="theme-color" content="#000" />
-          <meta name="description" content="this is myapp" />
+          <meta name="description" content="this is PWA LIST" />
           <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicons/favicon.ico" />
           <link rel="manifest" href="/favicons/manifest.json" />
           {/* font */}
           <link
