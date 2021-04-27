a3deb7b79f4711f41cdcea6513f48aa5fc06b3ff
MasakiFukunishi <fmsknn@gmail.com>
2021-04-27 20:06:54 +0900

    add page changed

--- pages/add.tsx
+++ pages/add.tsx
@@ -414,7 +414,7 @@ const add = () => {
             <ErrorMessage errors={errors.icon}></ErrorMessage>
           </div>
           <label className="block font-bold mb-2">
-            PC size screenshot (Up to 3 Images)
+            PC size screenshots (Up to 3 Images)
           </label>
           <div className="flex overflow-scroll">
             {pcImageUrlList.map((pcImageUrl, index) => (
@@ -438,7 +438,7 @@ const add = () => {
             </label>
           </div>
           <label className="block font-bold mb-2">
-            Mobile size screenshot (Up to 3 Images)
+            Mobile size screenshots (Up to 3 Images)
           </label>
           <div className="flex overflow-scroll">
             {mobileImageUrlList.map((mobileImageUrl, index) => (
