3f02b8be996565e7d6e50c172942ff7b420cb2d2
MasakiFukunishi <fmsknn@gmail.com>
2021-05-07 23:55:01 +0900

    added loading to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,15 @@ import { useState, useEffect } from "react";
 import firebase from "../plugins/firebase";
 import "firebase/firestore";
 import ApplicationCard from "../components/ApplicationCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 const db = firebase.firestore();
 const logo = {
   fontFamily: "'Nunito', sans-serif",
 };
 const Index = () => {
   const [newApps, setNewApps] = useState<Object | null>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const date = new Date();
   useEffect(() => {
     const fetchNewAppData = async () => {
@@ -32,6 +35,7 @@ const Index = () => {
           description: doc.data().description,
         }))
       );
+      setIsLoading(false);
     };
     fetchNewAppData();
   }, []);
@@ -41,40 +45,48 @@ const Index = () => {
         <h1 className="text-3xl font-bold mt-3" style={logo}>
           P<span className="text-green-500">W</span>A LIST
         </h1>
-        <div className="text-xl font-bold mt-6">New Apps</div>
-        <div className="mt-2">
-          <ApplicationCard applications={newApps} />
-        </div>
-        <div className="flex flex-col items-center text-center mt-7 md:hidden">
-          <Link href="/about" as="/about">
-            <a className="py-1 w-48 mb-3 text-gray-50 bg-gray-600 shadow-md rounded-md hover:bg-gray-700 hover:shadow-none transition ease-in-out">
-              <strong>What is a PWA</strong>
-            </a>
-          </Link>
-          <a
-            className="py-1 w-48 mb-3 text-gray-50 bg-blue-400 shadow-md rounded-md hover:bg-blue-500 hover:shadow-none transition ease-in-out"
-            target="_blank"
-            href="https://masakifukunishi.site"
-          >
-            <strong>Contact</strong>
-          </a>
-          <Link href="/add" as="/add">
-            <a className="py-1 w-48 mb-6 text-gray-50 bg-green-400 shadow-md rounded-md hover:bg-green-500 hover:shadow-none transition ease-in-out">
-              <strong>Submit Your App</strong>
-            </a>
-          </Link>
-          <p className="text-sm">
-            Made By&nbsp;
-            <a
-              className="text-green-500"
-              target="_blank"
-              href="https://masakifukunishi.site"
-            >
-              Masaki
-            </a>
-          </p>
-          <p className="text-xs">&copy; PWA List {date.getFullYear()}</p>
-        </div>
+        {isLoading ? (
+          <div className="text-center mt-24">
+            <FontAwesomeIcon icon={faSpinner} size="4x" className="fa-spin" />
+          </div>
+        ) : (
+          <div>
+            <div className="text-xl font-bold mt-6">New Apps</div>
+            <div className="mt-2">
+              <ApplicationCard applications={newApps} />
+            </div>
+            <div className="flex flex-col items-center text-center mt-7 md:hidden">
+              <Link href="/about" as="/about">
+                <a className="py-1 w-48 mb-3 text-gray-50 bg-gray-600 shadow-md rounded-md hover:bg-gray-700 hover:shadow-none transition ease-in-out">
+                  <strong>What is a PWA</strong>
+                </a>
+              </Link>
+              <a
+                className="py-1 w-48 mb-3 text-gray-50 bg-blue-400 shadow-md rounded-md hover:bg-blue-500 hover:shadow-none transition ease-in-out"
+                target="_blank"
+                href="https://masakifukunishi.site"
+              >
+                <strong>Contact</strong>
+              </a>
+              <Link href="/add" as="/add">
+                <a className="py-1 w-48 mb-6 text-gray-50 bg-green-400 shadow-md rounded-md hover:bg-green-500 hover:shadow-none transition ease-in-out">
+                  <strong>Submit Your App</strong>
+                </a>
+              </Link>
+              <p className="text-sm">
+                Made By&nbsp;
+                <a
+                  className="text-green-500"
+                  target="_blank"
+                  href="https://masakifukunishi.site"
+                >
+                  Masaki
+                </a>
+              </p>
+              <p className="text-xs">&copy; PWA List {date.getFullYear()}</p>
+            </div>
+          </div>
+        )}
       </div>
     </Layout>
   );
