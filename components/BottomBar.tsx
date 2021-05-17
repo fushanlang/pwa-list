58b2f8036bba52318e2774f1b90e3893935c9876
MasakiFukunishi <fmsknn@gmail.com>
2021-05-17 21:31:48 +0900

    change bottom bar

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -8,31 +8,31 @@ const BottomBar = () => {
   const path = router.pathname;
   return (
     <div className="bottom-0 w-full border-t bg-white fixed md:hidden">
-      <ul className="flex h-14 justify-around items-center">
-        <li className="text-center px-6">
+      <ul className="flex h-14 justify-around items-center pt-1">
+        <li className="text-center pr-6">
           <Link href="/">
             {path === "/" ? (
-              <a className="block px-8 text-green-600">
+              <a className="block text-green-600">
                 <FontAwesomeIcon icon={faHome} size="lg" className="" />
                 <span className="text-xs block">Home</span>
               </a>
             ) : (
-              <a className="block px-8 transition duration-500 ease-in-out hover:text-green-600">
+              <a className="block transition duration-500 ease-in-out hover:text-green-600">
                 <FontAwesomeIcon icon={faHome} size="lg" className="" />
                 <span className="text-xs block">Home</span>
               </a>
             )}
           </Link>
         </li>
-        <li className="text-center px-6">
+        <li className="text-center">
           <Link href="/search">
             {path === "/search" ? (
-              <a className="block px-8 text-green-600">
+              <a className="block text-green-600">
                 <FontAwesomeIcon icon={faSearch} size="lg" className="" />
                 <span className="text-xs block">Search</span>
               </a>
             ) : (
-              <a className="block px-8 transition duration-500 ease-in-out hover:text-green-600">
+              <a className="block transition duration-500 ease-in-out hover:text-green-600">
                 <FontAwesomeIcon icon={faSearch} size="lg" className="" />
                 <span className="text-xs block">Search</span>
               </a>
@@ -42,12 +42,12 @@ const BottomBar = () => {
         <li className="text-center">
           <Link href="/categories">
             {path === "/categories" ? (
-              <a className="block px-8 text-green-600">
+              <a className="block text-green-600">
                 <FontAwesomeIcon icon={faListUl} size="lg" className="" />
                 <span className="text-xs block">Categories</span>
               </a>
             ) : (
-              <a className="block px-8 transition duration-500 ease-in-out hover:text-green-600">
+              <a className="block transition duration-500 ease-in-out hover:text-green-600">
                 <FontAwesomeIcon icon={faListUl} size="lg" className="" />
                 <span className="text-xs block">Categories</span>
               </a>
