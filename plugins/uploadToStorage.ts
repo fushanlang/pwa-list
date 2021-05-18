b257b78088bf98b9a75436c2a688b05b14f4eb33
MasakiFukunishi <fmsknn@gmail.com>
2021-05-19 08:08:49 +0900

    change storage

--- plugins/uploadToStorage.ts
+++ plugins/uploadToStorage.ts
@@ -1,7 +1,7 @@
 import firebase from "../plugins/firebase";
 import "firebase/storage";
 const storage = firebase.storage();
-const uploadToImagesStorage = async (
+const uploadToStorage = async (
   folder: string,
   name: string,
   image: File,
@@ -10,12 +10,12 @@ const uploadToImagesStorage = async (
   if (image == null) {
     return null;
   }
-  await storage.ref(`${folder}/${name}_${fileName}`).put(image);
+  await storage.ref(`${folder}/${name}/${fileName}`).put(image);
   let res = await storage
     .ref(`${folder}`)
-    .child(`${name}_${fileName}`)
+    .child(`${name}/${fileName}`)
     .getDownloadURL();
   return res;
 };
 
-export default uploadToImagesStorage;
+export default uploadToStorage;
