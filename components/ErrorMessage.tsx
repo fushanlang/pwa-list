964ca96e47ca6dc782fc45618c796b4cf3c03e40
MasakiFukunishi <fmsknn@gmail.com>
2021-05-21 21:41:39 +0900

    Change the size of error messages.

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -3,7 +3,7 @@ const ErrorMessage = ({ errors }) => {
     <div className="mt-1">
       {errors !== null &&
         errors.map((error) => (
-          <p className="text-red-400" key={error}>
+          <p className="text-base text-red-400" key={error}>
             {error}
           </p>
         ))}
