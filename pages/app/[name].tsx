67ca291ae0a66c24340600e501a95b04ae8c4525
MasakiFukunishi <fmsknn@gmail.com>
2021-05-31 23:49:48 +0900

    maintenance add

--- pages/app/[name].tsx
+++ pages/app/[name].tsx
@@ -40,179 +40,190 @@ const ApplicationName = (applicationData) => {
     }
   }, []);
   return (
-    <Layout title={application.name}>
-      <Head>
-        <meta name="description" content={application.description} />
-        <meta key="og:title" property="og:title" content={application.name} />
-        <meta
-          key="og:site_name"
-          property="og:site_name"
-          content={application.name}
-        />
-        <meta key="og:url" property="og:url" content={url} />
-        <meta key="og:image" property="og:image" content={application.icon} />
-        <meta property="og:type" content="website" />
-        <meta
-          key="og:description"
-          property="og:description"
-          content={application.description}
-        />
-        <meta key="twitter:card" property="twitter:card" content="summary" />
-      </Head>
+    <Layout>
+      {" "}
       <div>
-        {application.name === undefined ? (
-          <NotFound />
-        ) : (
-          <div className="bg-white px-4 py-7 rounded-lg">
-            {existsBackPage ? (
-              <button
-                className="text-center mb-5 py-1 px-5 inline-block tracking-wide border-2 border-black bg-white shadow-md rounded-md hover:bg-gray-200 hover:shadow-none transition ease-in-out"
-                onClick={() => router.back()}
-              >
-                <FontAwesomeIcon icon={faChevronLeft} />
-                &nbsp;Back
-              </button>
-            ) : (
-              <Link href="/" as="/">
-                <button className="text-center mb-5 py-1 px-5 inline-block tracking-wide border-2 border-black bg-white shadow-md rounded-md hover:bg-gray-200 hover:shadow-none transition ease-in-out">
-                  <FontAwesomeIcon icon={faChevronLeft} />
-                  &nbsp;Back
-                </button>
-              </Link>
-            )}
-            <div className="flex items-center ml-1">
-              <div className="mr-4 w-20">
-                <img
-                  className="rounded-md"
-                  src={application.icon || "/default-app-icon.png"}
-                />
-              </div>
-              <div className="flex flex-col">
-                <h1 className="font-bold text-2xl ml-1">{application.name}</h1>
-                <h2 className="text-base ml-1">{caategoryFirstUpperCase}</h2>
-                <div className="mt-1">
-                  {application.tag1 && (
-                    <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
-                      {application.tag1}
-                    </span>
-                  )}
-                  {application.tag2 && (
-                    <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
-                      {application.tag2}
-                    </span>
-                  )}
-                  {application.tag3 && (
-                    <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
-                      {application.tag3}
-                    </span>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div>
-              <a
-                target="_blank"
-                // rel="noopener noreferrer"
-                href={application.link}
-                className="text-center w-60 mt-7 ml-1 py-1 inline-block tracking-wide text-gray-50 bg-green-400 shadow-md rounded-md hover:bg-green-500 hover:shadow-none transition ease-in-out"
-              >
-                VIEW&nbsp;
-                <FontAwesomeIcon icon={faLink} />
-              </a>
-            </div>
-            <div className="flex mt-6 overflow-scroll">
-              {application.imageMobile1 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[0]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer"
-                  src={application.imageMobile1}
-                />
-              )}
-              {application.imageMobile2 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[1]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer"
-                  src={application.imageMobile2}
-                />
-              )}
-              {application.imageMobile3 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[2]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer"
-                  src={application.imageMobile3}
-                />
-              )}
-              {application.imagePc1 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[3]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block"
-                  src={application.imagePc1}
-                />
-              )}
-              {application.imagePc2 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[4]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block "
-                  src={application.imagePc2}
-                />
-              )}
-              {application.imagePc3 !== null && (
-                <img
-                  onClick={() => {
-                    setModalIsOpen(true);
-                    setInitialSlide(slideNum[5]);
-                  }}
-                  className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block "
-                  src={application.imagePc3}
-                />
-              )}
-            </div>
-            <div className="mt-7 px-4">
-              <h3 className="text-left font-bold text-xl mb-2">
-                About this app
-              </h3>
-              <p
-                style={{ whiteSpace: "pre-line" }}
-                className="text-left text-lg"
-              >
-                {application.description}
-              </p>
-            </div>
-            <ImageModal
-              application={application}
-              modalIsOpen={modalIsOpen}
-              setModalIsOpen={setModalIsOpen}
-              initialSlide={initialSlide}
-            />
-          </div>
-        )}
-        {/* Google Adsense */}
-        <div className="overflow-scroll text-center mt-7">
-          <AdSense.Google
-            client={GOOGLE_ADSENSE_CLIENT}
-            slot="6767679949"
-            style={{ width: 970, height: 90 }}
-            format=""
-          />
-        </div>
-        {/* Google Adsense */}
+        <p className="text-2xl text-yellow-500 mt-6 mb-1">
+          We are currently undergoing maintenance.
+        </p>
+        <p className="text-2xl text-yellow-500">
+          We apologize for the inconvenience, and thank you for your patience.
+        </p>
       </div>
     </Layout>
+    // <Layout title={application.name}>
+    //   <Head>
+    //     <meta name="description" content={application.description} />
+    //     <meta key="og:title" property="og:title" content={application.name} />
+    //     <meta
+    //       key="og:site_name"
+    //       property="og:site_name"
+    //       content={application.name}
+    //     />
+    //     <meta key="og:url" property="og:url" content={url} />
+    //     <meta key="og:image" property="og:image" content={application.icon} />
+    //     <meta property="og:type" content="website" />
+    //     <meta
+    //       key="og:description"
+    //       property="og:description"
+    //       content={application.description}
+    //     />
+    //     <meta key="twitter:card" property="twitter:card" content="summary" />
+    //   </Head>
+    //   <div>
+    //     {application.name === undefined ? (
+    //       <NotFound />
+    //     ) : (
+    //       <div className="bg-white px-4 py-7 rounded-lg">
+    //         {existsBackPage ? (
+    //           <button
+    //             className="text-center mb-5 py-1 px-5 inline-block tracking-wide border-2 border-black bg-white shadow-md rounded-md hover:bg-gray-200 hover:shadow-none transition ease-in-out"
+    //             onClick={() => router.back()}
+    //           >
+    //             <FontAwesomeIcon icon={faChevronLeft} />
+    //             &nbsp;Back
+    //           </button>
+    //         ) : (
+    //           <Link href="/" as="/">
+    //             <button className="text-center mb-5 py-1 px-5 inline-block tracking-wide border-2 border-black bg-white shadow-md rounded-md hover:bg-gray-200 hover:shadow-none transition ease-in-out">
+    //               <FontAwesomeIcon icon={faChevronLeft} />
+    //               &nbsp;Back
+    //             </button>
+    //           </Link>
+    //         )}
+    //         <div className="flex items-center ml-1">
+    //           <div className="mr-4 w-20">
+    //             <img
+    //               className="rounded-md"
+    //               src={application.icon || "/default-app-icon.png"}
+    //             />
+    //           </div>
+    //           <div className="flex flex-col">
+    //             <h1 className="font-bold text-2xl ml-1">{application.name}</h1>
+    //             <h2 className="text-base ml-1">{caategoryFirstUpperCase}</h2>
+    //             <div className="mt-1">
+    //               {application.tag1 && (
+    //                 <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
+    //                   {application.tag1}
+    //                 </span>
+    //               )}
+    //               {application.tag2 && (
+    //                 <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
+    //                   {application.tag2}
+    //                 </span>
+    //               )}
+    //               {application.tag3 && (
+    //                 <span className="text-xs px-2 py-1 mr-2 rounded bg-gray-600 text-white">
+    //                   {application.tag3}
+    //                 </span>
+    //               )}
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <a
+    //             target="_blank"
+    //             // rel="noopener noreferrer"
+    //             href={application.link}
+    //             className="text-center w-60 mt-7 ml-1 py-1 inline-block tracking-wide text-gray-50 bg-green-400 shadow-md rounded-md hover:bg-green-500 hover:shadow-none transition ease-in-out"
+    //           >
+    //             VIEW&nbsp;
+    //             <FontAwesomeIcon icon={faLink} />
+    //           </a>
+    //         </div>
+    //         <div className="flex mt-6 overflow-scroll">
+    //           {application.imageMobile1 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[0]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer"
+    //               src={application.imageMobile1}
+    //             />
+    //           )}
+    //           {application.imageMobile2 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[1]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer"
+    //               src={application.imageMobile2}
+    //             />
+    //           )}
+    //           {application.imageMobile3 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[2]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer"
+    //               src={application.imageMobile3}
+    //             />
+    //           )}
+    //           {application.imagePc1 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[3]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block"
+    //               src={application.imagePc1}
+    //             />
+    //           )}
+    //           {application.imagePc2 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[4]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block "
+    //               src={application.imagePc2}
+    //             />
+    //           )}
+    //           {application.imagePc3 !== null && (
+    //             <img
+    //               onClick={() => {
+    //                 setModalIsOpen(true);
+    //                 setInitialSlide(slideNum[5]);
+    //               }}
+    //               className="border rounded max-h-96 mx-2 cursor-pointer hidden lg:inline-block "
+    //               src={application.imagePc3}
+    //             />
+    //           )}
+    //         </div>
+    //         <div className="mt-7 px-4">
+    //           <h3 className="text-left font-bold text-xl mb-2">
+    //             About this app
+    //           </h3>
+    //           <p
+    //             style={{ whiteSpace: "pre-line" }}
+    //             className="text-left text-lg"
+    //           >
+    //             {application.description}
+    //           </p>
+    //         </div>
+    //         <ImageModal
+    //           application={application}
+    //           modalIsOpen={modalIsOpen}
+    //           setModalIsOpen={setModalIsOpen}
+    //           initialSlide={initialSlide}
+    //         />
+    //       </div>
+    //     )}
+    //     {/* Google Adsense */}
+    //     <div className="overflow-scroll text-center mt-7">
+    //       <AdSense.Google
+    //         client={GOOGLE_ADSENSE_CLIENT}
+    //         slot="6767679949"
+    //         style={{ width: 970, height: 90 }}
+    //         format=""
+    //       />
+    //     </div>
+    //     {/* Google Adsense */}
+    //   </div>
+    // </Layout>
   );
 };
 
