102f682b7c6d4ae395e6f5e87fd7a1a236f5d677
MasakiFukunishi <fmsknn@gmail.com>
2022-04-16 21:14:24 +0900

    test cors

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = withPWA({
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
