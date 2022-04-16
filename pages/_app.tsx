1abfc25be6a0add8ad195db6afa3187b203bf184
MasakiFukunishi <fmsknn@gmail.com>
2022-04-17 00:42:47 +0900

    changed Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,45 @@ import { AppProps } from "next/app";
 import Router from "next/router";
 import * as gtag from "../plugins/gtag";
 import { AuthProvider } from "../contexts/Auth";
+import Head from "next/head";
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
+
+const title = "PWA List";
+const url = "https://pwalist.app/";
+const description = "Find your favorite progressive web apps here";
+const imageUrl =
+  "https://firebasestorage.googleapis.com/v0/b/pwa-list-b9174.appspot.com/o/common%2Fpwalist.png?alt=media&token=2247b98b-24d0-4304-92ad-f498607f0dcc";
+
 const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        <meta key="og:title" property="og:title" content={title} />
+        <meta key="og:site_name" property="og:site_name" content={title} />
+        <meta key="og:url" property="og:url" content={url} />
+        <meta key="og:image" property="og:image" content={imageUrl} />
+        <meta property="og:type" content="website" />
+        <meta
+          key="og:description"
+          property="og:description"
+          content={description}
+        />
+        <meta
+          key="twitter:card"
+          property="twitter:card"
+          content="summary_large_image"
+        />
+        <meta name="twitter:site" content="@masakifukunishi" />
+        {/* font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap"
+        />
+      </Head>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   );
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,11 +8,7 @@ import Document, {
 } from "next/document";
 import { GA_TRACKING_ID } from "../plugins/gtag";
 import { GOOGLE_ADSENSE_CLIENT } from "../plugins/googleAdsense";
-const title = "PWA List";
-const url = "https://pwalist.app/";
 const description = "Find your favorite progressive web apps here";
-const imageUrl =
-  "https://firebasestorage.googleapis.com/v0/b/pwa-list-b9174.appspot.com/o/common%2Fpwalist.png?alt=media&token=2247b98b-24d0-4304-92ad-f498607f0dcc";
 
 class MyDocument extends Document {
   static async getInitialProps(
@@ -89,27 +85,6 @@ class MyDocument extends Document {
             key="keywords"
             name="keywords"
             content="pwa, Progressive Web Apps"
-          />
-          <meta key="og:title" property="og:title" content={title} />
-          <meta key="og:site_name" property="og:site_name" content={title} />
-          <meta key="og:url" property="og:url" content={url} />
-          <meta key="og:image" property="og:image" content={imageUrl} />
-          <meta property="og:type" content="website" />
-          <meta
-            key="og:description"
-            property="og:description"
-            content={description}
-          />
-          <meta
-            key="twitter:card"
-            property="twitter:card"
-            content="summary_large_image"
-          />
-          <meta name="twitter:site" content="@masakifukunishi" />
-          {/* font */}
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap"
           />
         </Head>
         <body>
