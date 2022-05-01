411871aa4a09144fb53a1a6277d1e30a00e053d4
MasakiFukunishi <fmsknn@gmail.com>
2022-05-01 22:53:12 +0900

    fixed dark mode

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -4,6 +4,10 @@ import { AuthContext } from "../../contexts/Auth";
 import Layout from "../../components/Layout/Layout";
 import Auth from "../../components/SignUp/Auth";
 import Router from "next/router";
+const logo = {
+  fontFamily: "'Nunito', sans-serif",
+};
+
 const SignUp: NextPage = () => {
   const { currentUser } = useContext(AuthContext);
   useEffect(() => {
@@ -13,8 +17,10 @@ const SignUp: NextPage = () => {
     <Layout title="Sign-Up">
       {!currentUser && (
         <div className="text-center mt-3 pt-10 pb-7 ">
-          <img className="w-36 inline-block mb-3" src="/icon-for-sign-up.png" />
-          <h1 className="text-2xl font-bold mb-1">Sign up on PWA List</h1>
+          <h1 className="text-3xl font-bold mb-7" style={logo}>
+            P<span className="text-green-500">W</span>A LIST
+          </h1>
+          <h2 className="text-2xl font-bold mb-3">Sign up on PWA List</h2>
           <p className="text-lg mb-5">In order to add and manage your apps, please sign up for an account.</p>
           <Auth></Auth>
         </div>
