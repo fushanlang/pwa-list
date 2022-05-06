a21fffeff367515e91495e30a7366a878e77a5e2
MasakiFukunishi <fmsknn@gmail.com>
2022-05-06 22:56:51 +0900

    changed header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 const title = "PWA List";
 const url = "https://pwalist.app/";
-const description = "Find your favorite progressive web apps here";
+const description = "In PWA List, you can search for Progressive Web Apps by tag, category, and app name.";
 const imageUrl =
   "https://firebasestorage.googleapis.com/v0/b/pwa-list-b9174.appspot.com/o/common%2Fpwalist.png?alt=media&token=2247b98b-24d0-4304-92ad-f498607f0dcc";
 
@@ -20,6 +20,10 @@ const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
+        {/* common */}
+        <title>PWA List - Progressive Web Apps List</title>
+        <meta name="description" content={description} />
+        <meta key="keywords" name="keywords" content="pwa, Progressive Web Apps" />
         <meta key="og:title" property="og:title" content={title} />
         <meta key="og:site_name" property="og:site_name" content={title} />
         <meta key="og:url" property="og:url" content={url} />
@@ -28,6 +32,22 @@ const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta key="og:description" property="og:description" content={description} />
         <meta key="twitter:card" property="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@masakifukunishi" />
+        <meta name="application-name" content="PWA List" />
+        <meta name="theme-color" content="#000" />
+        <link rel="icon" sizes="192x192" href="/favicons/icon-192x192.png" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        {/* windows */}
+        <meta name="msapplication-square70x70logo" content="/favicons/site-tile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="/favicons/site-tile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="/favicons/site-tile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="/favicons/site-tile-310x310.png" />
+        <meta name="msapplication-TileColor" content="#000" />
+        {/* safari */}
+        <meta name="/faviconsapple-mobile-web-app-capable" content="yes" />
+        <meta name="/faviconsapple-mobile-web-app-status-bar-style" content="#000" />
+        <meta name="/faviconsapple-mobile-web-app-title" content="PWA List" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png" />
         {/* font */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" />
       </Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
 import { GA_TRACKING_ID } from "../plugins/gtag";
 import { GOOGLE_ADSENSE_CLIENT } from "../plugins/googleAdsense";
-const description = "Find your favorite progressive web apps here";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -32,25 +31,6 @@ class MyDocument extends Document {
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script> */}
-          {/* windows */}
-          <meta name="msapplication-square70x70logo" content="/favicons/site-tile-70x70.png" />
-          <meta name="msapplication-square150x150logo" content="/favicons/site-tile-150x150.png" />
-          <meta name="msapplication-wide310x150logo" content="/favicons/site-tile-310x150.png" />
-          <meta name="msapplication-square310x310logo" content="/favicons/site-tile-310x310.png" />
-          <meta name="msapplication-TileColor" content="#000" />
-          {/* safari */}
-          <meta name="/faviconsapple-mobile-web-app-capable" content="yes" />
-          <meta name="/faviconsapple-mobile-web-app-status-bar-style" content="#000" />
-          <meta name="/faviconsapple-mobile-web-app-title" content="PWA List" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png" />
-          {/* common */}
-          <meta name="application-name" content="PWA List" />
-          <meta name="theme-color" content="#000" />
-          <link rel="icon" sizes="192x192" href="/favicons/icon-192x192.png" />
-          <link rel="icon" href="/favicons/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content={description} />
-          <meta key="keywords" name="keywords" content="pwa, Progressive Web Apps" />
         </Head>
         <body className="text-sm font-sans text-gray-600 dark:text-gray-200 dark:bg-gray-800">
           <Main />
