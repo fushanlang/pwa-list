f1cd023cdcf328a6e183e38aefe778485af67f44
MasakiFukunishi <fmsknn@gmail.com>
2022-05-12 21:56:35 +0900

    changed name validation

--- plugins/validation/validateAlphanum.ts
+++ plugins/validation/validateAlphanum.ts
@@ -1,7 +1,7 @@
 const validateAlphanum = (property: string) => {
   if (property === "" || property === null) return false;
 
-  var result = property.match(/^[A-Za-z0-9\s]*$/);
+  var result = property.match(/^[A-Za-z0-9-\s]*$/);
   const error = result === null ? true : false;
   return error;
 };
