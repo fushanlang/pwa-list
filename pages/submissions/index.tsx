2547e25ab512ef37282c7914d29c36d8cb6a20b8
MasakiFukunishi <fmsknn@gmail.com>
2022-07-27 23:07:35 +0900

    delete import useContext

--- pages/submissions/index.tsx
+++ pages/submissions/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import Link from "next/link";
 import Router from "next/router";
