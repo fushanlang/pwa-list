9336e069451269e666d72040b8a5f2c580022cff
MasakiFukunishi <fmsknn@gmail.com>
2022-08-08 22:11:50 +0900

    changed image tag

--- pages/about.tsx
+++ pages/about.tsx
@@ -39,9 +39,9 @@ const About: NextPage = () => {
             the case of Chrome.
           </p>
           <div className="mt-4 ml-2 mb-1">
-            <img style={{ height: "530px" }} src={"/about/mobile1.jpg"} className="mr-8 mb-3 rounded-md inline" />
-            <img style={{ maxHeight: "530px" }} src={"/about/mobile2.jpg"} className="mr-8 mb-3 rounded-md inline" />
-            <img style={{ maxHeight: "530px" }} src={"/about/mobile3.jpg"} className="mr-8 mb-3 rounded-md inline" />
+            <img style={{ height: "530px" }} alt="screenshot1" src={"/about/mobile1.jpg"} className="mr-8 mb-3 rounded-md inline" />
+            <img style={{ height: "530px" }} alt="screenshot2" src={"/about/mobile2.jpg"} className="mr-8 mb-3 rounded-md inline" />
+            <img style={{ height: "530px" }} alt="screenshot3" src={"/about/mobile3.jpg"} className="mr-8 mb-3 rounded-md inline" />
           </div>
           <p>The procedure is slightly different depending on the browser, but it is almost the same.</p>
         </div>
