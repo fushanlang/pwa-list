0a35313e50022c7cb7bd0de82232fbccd068e8a9
MasakiFukunishi <fmsknn@gmail.com>
2022-08-25 00:18:18 +0900

    removed ISR

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -64,7 +64,7 @@ export async function getStaticProps(context) {
       apps: apps,
       category: changeFirstUpperCase(category),
     },
-    revalidate: 20,
+    // revalidate: 20,
   };
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,7 @@ export async function getStaticProps() {
 
   return {
     props: { apps },
-    revalidate: 20,
+    // revalidate: 20,
   };
 }
 
