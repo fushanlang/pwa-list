b842d986f6cb604f0dd350c16902f591ddfde479
MasakiFukunishi <fmsknn@gmail.com>
2022-08-30 22:24:37 +0900

    changed login to popup

--- components/SignUp/LoginWithOtherService.tsx
+++ components/SignUp/LoginWithOtherService.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 type Props = {
   label: string;
   imagePath: string;
-  handleLogin: () => void;
+  handleSignIn: () => void;
 };
 
-const LoginButton: React.FC<Props> = ({ label, imagePath, handleLogin }) => {
+const SignInWithOtherService: React.FC<Props> = ({ label, imagePath, handleSignIn }) => {
   return (
     <button
       className="flex items-center justify-center mx-2 my-2 w-56 h-10 font-bold border rounded hover:bg-gray-100 dark:hover:bg-gray-700"
-      onClick={handleLogin}
+      onClick={handleSignIn}
     >
       <div className="mr-3 w-6 pt-1">
         <Image alt={label} src={imagePath} width={50} height={50} objectFit="contain" />
@@ -19,4 +19,4 @@ const LoginButton: React.FC<Props> = ({ label, imagePath, handleLogin }) => {
     </button>
   );
 };
-export default LoginButton;
+export default SignInWithOtherService;
