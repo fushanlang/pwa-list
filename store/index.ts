dc5b2d7c8dfecb0523e150173f46b69b33f96a37
MasakiFukunishi <fmsknn@gmail.com>
2022-08-31 01:05:13 +0900

    fixed userApps

--- store/index.ts
+++ store/index.ts
@@ -10,7 +10,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ["userApps/setAsyncApps/fulfilled"],
+        ignoredActions: ["userApps/set"],
       },
     }),
 });
--- store/modules/userApps.ts
+++ store/modules/userApps.ts
@@ -1,51 +1,35 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import "firebase/firestore";
 
 import { RootState } from "../index";
-import firebase from "../../plugins/firebase";
 
 const userApps = createSlice({
   name: "userApps",
   initialState: {
-    isLoading: true,
     apps: [],
   },
   reducers: {
+    set(state, { payload }) {
+      state.apps = payload.docs.map((doc) => ({
+        id: doc.id,
+        name: doc.data().name,
+        nameLowercase: doc.data().nameLowercase,
+        icon: doc.data().icon,
+        isPublic: doc.data().isPublic,
+        isRejected: doc.data().isRejected,
+        rejectionMessage: doc.data().rejectionMessage,
+      }));
+    },
     remove(state, { payload }) {
       state.apps = state.apps.filter((value) => {
         return value.id !== payload;
       });
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(setAsyncApps.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(setAsyncApps.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.apps = action.payload.map((doc) => ({
-          id: doc.id,
-          name: doc.data().name,
-          nameLowercase: doc.data().nameLowercase,
-          icon: doc.data().icon,
-          isPublic: doc.data().isPublic,
-          isRejected: doc.data().isRejected,
-          rejectionMessage: doc.data().rejectionMessage,
-        }));
-      });
-  },
 });
 
-const setAsyncApps = createAsyncThunk("userApps/setAsyncApps", async (payload: string) => {
-  const db = firebase.firestore();
-  const response = await db.collection("applications").where("userId", "==", payload).orderBy("updatedAt", "desc").get();
-  return response.docs;
-});
+const { set, remove } = userApps.actions;
 
-const { remove } = userApps.actions;
-
-export { remove, setAsyncApps };
+export { set, remove };
 export const selectUserApps = (state: RootState) => state.userApps.apps;
-export const selectIsLoading = (state: RootState) => state.userApps.isLoading;
 export default userApps.reducer;
