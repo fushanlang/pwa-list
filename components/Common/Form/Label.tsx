5da286915cba2b108a7b1a4e1ea7a07c152e37bb
MasakiFukunishi <fmsknn@gmail.com>
2022-09-15 21:46:50 +0900

    Change label

--- components/Common/Form/Label.tsx
+++ components/Common/Form/Label.tsx
@@ -8,13 +8,15 @@ const Label: React.FC<Props> = ({ id, label = "", labelMessage = "", isRequired 
   return (
     <>
       {label && (
-        <label className="block mb-2" htmlFor={id}>
-          <span className="font-bold">
-            {label}
-            {isRequired && <span className="text-red-400 ml-2">*</span>}
-          </span>
-          {labelMessage && <span className="ml-2">{labelMessage}</span>}
-        </label>
+        <div className="mb-2">
+          <label htmlFor={id}>
+            <span className="font-bold">
+              {label}
+              {isRequired && <span className="text-red-400 ml-2">*</span>}
+            </span>
+            {labelMessage && <span className="ml-2">{labelMessage}</span>}
+          </label>
+        </div>
       )}
     </>
   );
